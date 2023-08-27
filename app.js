ba200fd05cdad2b23726d0d8ec10152f3dd225e3
nigarnuraliyeva0226 <nikanurali0226@gmail.com>
2023-08-27 16:09:18 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+let a = +prompt("ayin nomreaini daxil edin");
+
+switch (a){
+    case 1:
+        console.log ("yanvar")
+     break
+     case 2:
+        console.log ("fevral")
+      break
+      case 3:
+        console.log ("mart")
+      break
+      case 4:
+        console.log ("aprel")
+      break
+      case 5:
+        console.log ("may")
+      break
+      case 6:
+        console.log ("iyun")
+      break
+      case 7:
+        console.log ("iyul")
+      break
+      case 8:
+        console.log ("avqust")
+      break
+      case 9:
+        console.log ("sentyabr")
+      break
+      case 10:
+        console.log ("oktyabr")
+      break
+      case 11:
+        console.log ("noyabr")
+      break
+      case 12:
+        console.log ("fevral")
+      break
+      default:
+        console.log ("yanlis deyer")
+
+
+}
